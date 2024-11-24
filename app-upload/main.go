bf29c93a588b7a119e8a27b0c9fc4d5a36576f7b
Felipe Azevedo <felipe.azevedo@cws.digital>
2024-11-24 14:08:04 -0300

    upload one file

--- app-upload/main.go
+++ app-upload/main.go
@@ -39,11 +39,12 @@ func uploadFiles(w http.ResponseWriter, r *http.Request) {
 	files, err := t.UploadFiles(r, "./uploads")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
 
 	out := ""
 	for _, item := range files {
-		out += fmt.Sprintf("Uploaded %s to the uploads folder, renamed to %s", item.OriginalFileName, item.NewFileName)
+		out += fmt.Sprintf("Uploaded %s to the uploads folder, renamed to %s\n", item.OriginalFileName, item.NewFileName)
 	}
 
 	_, _ = w.Write([]byte(out))
@@ -51,5 +52,20 @@ func uploadFiles(w http.ResponseWriter, r *http.Request) {
 }
 
 func uploadOneFile(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+	}
 
+	t := toolkit.Tools{
+		MaxFileSize:      1024 * 1024 * 1024,
+		AllowedFileTypes: []string{"image/jpeg", "image/png", "image/gif"},
+	}
+
+	f, err := t.UploadOneFile(r, "./uploads")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	_, _ = w.Write([]byte(fmt.Sprintf("Uploaded 1 file, %s, to the uploads folder", f.OriginalFileName)))
 }
