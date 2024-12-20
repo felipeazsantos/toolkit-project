7cb43cc0d0660474f9d2c6f7c9d3fcd0ec311898
Felipe Azevedo <felipe.azevedo@cws.digital>
2024-12-20 13:23:37 -0300

    testing WriteJSON func

--- toolkit/tools_test.go
+++ toolkit/tools_test.go
@@ -288,3 +288,21 @@ func TestTools_ReadJSON(t *testing.T) {
 		}
 	}
 }
+
+func TestTools_WriteJSON(t *testing.T) {
+	var testTool Tools
+
+	rr := httptest.NewRecorder()
+	payload := JSONResponse{
+		Error:   false,
+		Message: "foo",
+	}
+
+	headers := make(http.Header)
+	headers.Add("FOO", "BAR")
+
+	err := testTool.WriteJSON(rr, http.StatusOK, payload, headers)
+	if err != nil {
+		t.Errorf("failed to write JSON: %v", err)
+	}
+}
